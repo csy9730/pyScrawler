ef25669b243e588221d948672faa8dd632d9750b
csy9730 <csy9730@users.noreply.github.com>
2019-08-29 07:35:53 +0800

    tube meizitu

--- code/scrapy/scrapy_sample/scrapy_sample/spiders/meizitu.py
+++ code/scrapy/scrapy_sample/scrapy_sample/spiders/meizitu.py
@@ -6,16 +6,27 @@ from scrapy.http import Request
 class MeizituSpider(scrapy.Spider):
     name = 'meizitu'
     start_urls = [#'http://www.meizitu.org/page/2/',
-	'https://www.meizitu.com/a/5501.html',
-                  'https://www.meizitu.com/a/5524.html'
-				  ]
-    def start_requests(self):      
-        user_agent = 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36' 
-        header = { #'accept-encoding': 'gzip, deflate, br', 
+	'https://www.meizitu.com',
+               #   'https://www.meizitu.com/a/5524.html'
+	]
+    header = { #'accept-encoding': 'gzip, deflate, br', 
             #'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
             #'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'user_agent': user_agent}
-        return [Request(self.start_urls[0],headers=header,callback=self.parse)]
-    def parse(self, response):
+            'user_agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36' }
+    def start_requests(self):      
+        return [Request(self.start_urls[0],headers=self.header,callback=self.parse)]
+    def parse(self,response):
         print(response.request.headers)
+        urls = response.xpath('.//*[@id="wp_page_numbers"]/ul/li/@href').getall()
+        for url in urls:
+            print(url)
+            yield  Request(url ,headers=self.header,callback=self.parse_page)
+    def parse_page(self, response):
+        urls = response.xpath('li//h3[@class="tit"]/@href').getall()
+        for url in urls:
+            print(url)
+            yield  Request(url ,headers=self.header,callback=self.parse_img)
+    def parse_img(self, response):        
         yield ImageItem(image_urls=response.css('div#picture img::attr(src)').extract())
+
+
--- code/scrapy/scrapy_sample/scrapy_sample/start.py
+++ code/scrapy/scrapy_sample/scrapy_sample/start.py
@@ -3,6 +3,7 @@ from scrapy import cmdline
 
 # 续爬模式，会自动生成一个crawls文件夹，用于存放断点文件
 # cmdline.execute('scrapy crawl mySpider -s JOBDIR=crawls/storemySpider'.split())
-
+keys = "meizitu"
+cmd = 'scrapy crawl %s -a tieba=dota -o scr_mm131.jl -s CLOSESPIDER_ITEMCOUNT=400' % keys
 # 非续爬模式
-cmdline.execute('scrapy crawl meizitu -a tieba=dota -o scr_mm131.jl -s CLOSESPIDER_ITEMCOUNT=20'.split())
+cmdline.execute(cmd .split())
