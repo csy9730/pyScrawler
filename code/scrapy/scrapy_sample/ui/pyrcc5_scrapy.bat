26e9304ff23918fb50acaeb13f6b54fea6606d5a
csy9730 <csy9730@users.noreply.github.com>
2020-03-18 00:08:08 +0800

    add ui

--- code/scrapy/scrapy_sample/ui/pyrcc5_scrapy.bat
+++ code/scrapy/scrapy_sample/ui/pyrcc5_scrapy.bat
@@ -1,6 +1,11 @@
 pushd %~dp0
+start /b pyuic5 mainwindow3.ui -o ui_mainwidow3.py
+start /b pyuic5 formparam.ui -o ui_formparam.py
+start /b pyuic5 formsubparam.ui -o ui_formsubparam.py
+start /b pyuic5 formstart.ui -o ui_formstart.py
+start /b pyuic5 mainwindow.ui -o ui_mainwidow.py
+echo "pyuic5 finished"
 pyrcc5  pyscr.qrc -o  pyscr_rc.py
 echo "pyrcc5 finished"
-call pyuic5 mainwindow.ui -o ui_mainwidow.py
 echo "finished"
 popd
